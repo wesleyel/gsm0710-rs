bbe3630fbefff4bf639443f8bc68c80e51df8a15
Wesley Yang <48174882+wesleyel@users.noreply.github.com>
2024-08-30 02:48:41 +0000

    refactor: Update CLI arguments and comments for better clarity and consistency

--- src/cli.rs
+++ src/cli.rs
@@ -4,22 +4,22 @@ use serde::Serialize;
 #[derive(ValueEnum, Clone, Default, Debug, Serialize)]
 #[serde(rename_all = "kebab-case")]
 pub enum ModemType {
-    /// Generic modem
+    /// Init modem genericly
     #[default]
     Generic,
-    /// Sam201 modem
+    /// Init Sam201 modem
     Sam201,
 }
 
 /// A gsm0710 protocol MUX implementation
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about, author, long_about = None, arg_required_else_help = true)]
 pub struct Args {
     /// Pty device to open
     #[arg(short, long, default_value = "/dev/ptmx")]
     pub pty: String,
 
-    /// Number of channels to create.
+    /// Number of channels to create (0-63)
     #[arg(short, long, default_value = "7")]
     pub channels: u8,
 
@@ -35,7 +35,7 @@ pub struct Args {
     #[arg(short, long, default_value = "generic")]
     pub modem: ModemType,
 
-    /// Prefix for the symlinks (e.g. /dev/mux)
+    /// Create symlinks for pts. (e.g. /dev/mux)
     #[arg(short, long)]
     pub symlink_prefix: Option<String>,
 
@@ -47,7 +47,7 @@ pub struct Args {
     #[arg(short, long, action = ArgAction::SetTrue)]
     pub auto_restart: bool,
 
-    /// Verbose mode (-v, -vv, -vvv)
+    /// Verbose mode. (e.g. -v, -vv, -vvv)
     #[arg(short, long, action = ArgAction::Count)]
     pub verbose: u8,
 
